29896a08766176cc7ba5bcde216e15d64b7c11e5
Evgeni Suhinin <sewsk-boy@yandex.ru>
2023-07-09 18:28:15 +0300

    c#

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+int ReadInt (string text)
+{
+    System.Console.WriteLine ();
+    return Convert.ToInt32(Console.ReadLine);
+}
+int ResultPov (int A, int B);
+{
+    if (B == 0) return 1;
+    int count = 0;
+    int result = 1;
+    while (count < 0)
+    {
+        result *= A;
+        count++;
+    }
+    return result;
+}
+//----------------------------------------------
+
+int A = ReadInt("Число: ");
+int B = ReadInt("Степень: ");
+if (B < 0)
+{
+    System.Console.WriteLine("Вы ввели отрицательный показатель степени");
+}
+else
+{
+    System.Console.WriteLine($"Число {A} в степени {B} равно: {ResultPov(A, B)}");
+}
